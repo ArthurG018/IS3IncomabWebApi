66dadb8b57e20949dcb6ce1675508202bf2cb552
Arthur G <garciaayalaarthur@gmail.com>
2024-08-11 15:36:39 -0500

    ConnectionDatabase Carolina

--- IS3IncomabWebApi/Infraestructure/Data/ConnectionDataBase.cs
+++ IS3IncomabWebApi/Infraestructure/Data/ConnectionDataBase.cs
@@ -18,7 +18,7 @@ namespace IS3IncomabWebApi.Infraestructure.Data
             {
                 #pragma warning disable IDE0017
                 SqlConnection sqlConnection = new();
-                sqlConnection.ConnectionString = (sqlConnection.WorkstationId.Equals(_configuration.GetConnectionString("DesktopArthur"))) ? _configuration.GetConnectionString("ConnectionDB") : _configuration.GetConnectionString("ConnectionF");
+                sqlConnection.ConnectionString = (sqlConnection.WorkstationId.Equals(_configuration.GetConnectionString("DesktopArthur"))) ? _configuration.GetConnectionString("ConnectionDB") : _configuration.GetConnectionString("ConnectionC");
                 sqlConnection.Open();
                 return sqlConnection;
             }
